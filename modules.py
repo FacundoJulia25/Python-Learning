080990dbad03559f762eee2fc4b4fbe1c792246f
facundojulia <facundo@meowtel.com>
2024-02-12 11:56:12 -0300

    modules first part of lesson #9

---
+++ modules.py
@@ -0,0 +1,8 @@
+import math
+from random import randint
+from string import ascii_lowercase,ascii_uppercase
+
+print(randint(0,10))
+print(ascii_lowercase + ascii_uppercase)
+
+print(round(math.sqrt(4)))
